10675b7614f0b2c3453c6bd8021d850429d059a1
dongrerohan421 <dongrerohan421@gmail.com>
2020-08-06 15:25:02 -0400

    returns the same string in lowercase

---
+++ src/to_lower_case/Solution.java
@@ -0,0 +1,7 @@
+package to_lower_case;
+
+public class Solution {
+    public String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
---
+++ src/to_lower_case/ToLowerCase.java
@@ -0,0 +1,11 @@
+package to_lower_case;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(solution.toLowerCase("Hello"));
+        System.out.println(solution.toLowerCase("here"));
+        System.out.println(solution.toLowerCase("LOVELY"));
+    }
+}
