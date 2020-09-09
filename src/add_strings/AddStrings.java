f03030fce83ffb46a3198f845a5eba249f613c3f
dongrerohan421 <dongrerohan421@gmail.com>
2020-09-09 14:26:35 -0400

    add string

---
+++ src/add_strings/AddStrings.java
@@ -0,0 +1,8 @@
+package add_strings;
+
+public class AddStrings {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.addStrings("0", "0"));
+    }
+}
---
+++ src/add_strings/Solution.java
@@ -0,0 +1,7 @@
+package add_strings;
+
+public class Solution {
+    public String addStrings(String num1, String num2) {
+        return num1 + num2;
+    }
+}
