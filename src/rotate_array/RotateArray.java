8861cd0697cd0e9e1f44edc9ea13767291b0d478
dongrerohan421 <dongrerohan421@gmail.com>
2020-09-09 14:26:55 -0400

    rotate array

---
+++ src/rotate_array/RotateArray.java
@@ -0,0 +1,20 @@
+package rotate_array;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 3;
+        rotate(nums, k);
+    }
+
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        for (int i = 0; i < k; i++) {
+            int temp = nums[nums.length - 1];
+            for (int j = nums.length - 1; j > 0; j--) {
+                nums[j] = nums[j - 1];
+            }
+            nums[0] = temp;
+        }
+    }
+}
