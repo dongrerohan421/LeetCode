6cc936188bdffa75113fcfd690483580e122f646
dongrerohan421 <dongrerohan421@gmail.com>
2020-09-28 14:51:45 -0400

    DuplicateZeros

---
+++ src/duplicate_zeros/Solution.java
@@ -0,0 +1,10 @@
+package duplicate_zeros;
+
+public class Solution {
+    public static void main(String[] args) {
+        DuplicateZeros duplicateZeros = new DuplicateZeros();
+        int[] input = { 1, 0, 2, 3, 0, 4, 5, 0 };
+
+        duplicateZeros.duplicateZeros(input);
+    }
+}
