0f62119091fc4102a0a25c2944efdbbeb43100f0
dongrerohan421 <dongrerohan421@gmail.com>
2020-10-05 14:48:50 -0400

    implemented sorting algo

--- src/second_largest_number_array/SecondLargestNumber.java
+++ src/second_largest_number_array/SecondLargestNumber.java
@@ -1,11 +1,22 @@
 package second_largest_number_array;
 
-import java.util.Arrays;
-
 public class SecondLargestNumber {
     public int secondLargestNumber(int[] arr) {
-        Arrays.sort(arr);
 
+        int temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
         return arr[arr.length - 1 - 1];
     }
 }
