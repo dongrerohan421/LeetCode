8202577a532516f8870e63fd8e3aaa8a345a625b
Rohan Dongre <dongrerohan421@gmail.com>
2026-09-10 16:26:08 -0700

    add twoSum hashMapSolution

--- src/two_sum/TwoSum.java
+++ src/two_sum/TwoSum.java
@@ -4,10 +4,12 @@ public class TwoSum {
 
 	public static void main(String[] args) {
 		Solution solution = new Solution();
+		HashMapSolution hashMapSolution = new HashMapSolution();
 
 		int[] nums = { 2, 7, 11, 15 };
 		int target = 9;
 
 		solution.twoSum(nums, target);
+		hashMapSolution.twoSum(nums, target);
 	}
 }
